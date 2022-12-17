ec1cc460fc9831abc2e7f1cf712d444aff2844b3
Lucas Morais <lucascdemorais@gmail.com>
2022-12-17 14:25:59 -0300

    starting to add unity of work pattern

--- backend/src/main/java/com/messageboard/persistence/TransactionHandler.java
+++ backend/src/main/java/com/messageboard/persistence/TransactionHandler.java
@@ -1,39 +1,40 @@
 package com.messageboard.persistence;
 
 import com.messageboard.utils.ThrowableSupplier;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.hibernate.TransactionException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
 
-@Service
-@Transactional
 public class TransactionHandler {
-    @PersistenceContext
     private final EntityManager entityManager;
     private EntityTransaction currentTransaction = null;
 
-    @Autowired
+
     public TransactionHandler(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
+
     public <T> T encapsulateTransaction(ThrowableSupplier<T> callback) throws Exception {
-        //EntityTransaction transaction = getTransaction();
+        EntityTransaction transaction = getTransaction();
         try {
-           // transaction.begin();
+            transaction.begin();
             T result = callback.get();
-           // transaction.commit();
+            transaction.commit();
             return result;
         } catch (Exception e) {
-            //transaction.rollback();
+            transaction.rollback();
             throw e;
         } finally {
             currentTransaction = null;
         }
+    }
+    
+    public void flush(){
+        if (currentTransaction == null || !currentTransaction.isActive()) throw new TransactionException("The transaction should be initiated before \"flush()\" method call");
+        currentTransaction.commit();
+        entityManager.flush();
     }
 
     private EntityTransaction getTransaction() {
